0b562797de2e48afb9835dcb515b12df2020b5c6
silverlily <lily@silverlily.net>
2020-10-22 21:48:16 -0600

    t

--- spindle-modules/command-handler.js
+++ spindle-modules/command-handler.js
@@ -1,6 +1,6 @@
 async function handler(bot,msg,guildCashe,db){
   if(msg.channel.type === 0){
-    console.log
+    console.log('prefix: '+guildCashe[msg['guildID']]['prefix'])
     if(msg.content.substring(0,(guildCashe[msg['guildID']]['prefix']).length) === guildCashe[msg['guildID']]['prefix']) {
       console.log('command: '+msg.content)
 
@@ -15,27 +15,15 @@ async function handler(bot,msg,guildCashe,db){
       switch (command) {
         case 'help':
           console.log('cmd: help')
-          bot.createMessage(msg.channel.id,"Join a voice channel and use "+guildCashe[msg['guildID']]['prefix'] + 'link in the channel you wish to link! The bot must have the ability to Manage Permissions for the text channel you link.\n \n Set the channel permissions to the permissions you want users to have in that channel, except with **View Channel** disabled. When a user joins the linked voice channel it will automaticly add a permission overwride for that user to view the channel, then remove that overwride when they leave. You can remove a channel link by following the same process used to create one.\n \nYou can set your servers prefix using [coming soon]')
-          break;
-        case 'ping':
-          console.log('cmd: ping')
-          bot.createMessage(msg.channel.id,"pong");
+          bot.createMessage(msg.channel.id,"Join a voice channel and use **"+guildCashe[msg['guildID']]['prefix'] + 'link** in the channel you wish to link! The bot must have the ability to Manage Permissions for the text channel you link.\n \n Set the channel permissions to the permissions you want users to have in that channel, except with **View Channel** disabled. When a user joins the linked voice channel it will automaticly add a permission overwride for that user to view the channel, then remove that overwride when they leave. You can remove a channel link by following the same process used to create one. **You can only use the link command if you have the admin permission.**\n \nYou can set your servers prefix using [coming soon]')
           break;
         case 'link':
           console.log('cmd: link')
           addVoiceLink(args,msg,guildCashe,bot,db)
           break;
-        case 'status':
-          console.log('cmd: status')
-          let options = {'userIDs':msg.author.id}
-          let guild = msg.channel.guild
-          let member = guild.fetchMembers(options);
-          member = (await member)[0];
-          console.log('status:\n'+JSON.stringify(member))
-          break;
-        case 'settings':
-          console.log("cmd: server")
-          break;
+        case 'prefix':
+          console.log('cmd: prefix')
+
       }
     }
   }
