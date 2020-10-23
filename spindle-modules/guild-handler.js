4b611d765c79760079ddf18746704974cec84781
silverlily <lily@silverlily.net>
2020-10-22 23:44:12 -0600

    t

--- spindle-modules/guild-handler.js
+++ spindle-modules/guild-handler.js
@@ -5,7 +5,7 @@ const defaultServerConfig = '{"prefix":"|","linkedChannels":{"enabled":true,"cha
 async function newGuild(guild,db,guildCashe,prefix){
   console.log("adding new guild")
   db.query('INSERT INTO servers (id,settings) VALUES ($1,$2)',[guild['id'],JSON.parse(defaultServerConfig)])
-  return newguild
+  return defaultServerConfig
 }
 
 exports.newGuild = newGuild;
