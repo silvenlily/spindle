d28f02d31da76b1297d504d60e887d48569259e2
silverlily <lily@silverlily.net>
2021-02-08 14:22:57 -0700

    fix bug from previous bug fix

--- Spindle.js
+++ Spindle.js
@@ -52,7 +52,7 @@ function startup(){ //connects to postgres, fetches the guild cashe, and connect
 bot.on("ready", async () => {
   console.log("connected!\nready!");
   console.log("setting status")
-  bot.editStatus("online",{name:"Undergoing maintenance, expect weirdness"})
+  bot.editStatus("online",{name:"for |help",type:2})
 });
 
 bot.on("guildCreate", async (guild) => { //adds a new guild to the cashe and storage when the bot joins a new guild
